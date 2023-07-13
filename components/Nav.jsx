3d7e1c1062cd68adbc5c2a8bdcaae74983d288e3
Shaikmahenoor024 <shaikmahenoor024@gmail.com>
2023-07-13 19:15:49 +0530

    changes in navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div>Sri Venkateswara University</div>
-  )
+    <div>
+      <div>
+        <a>Attendance Website</a>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/">Attendance Sheets</Link>
+          </li>
+          <li>
+            <Link href="/">About</Link>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  );
 }
 
 export default Navbar
